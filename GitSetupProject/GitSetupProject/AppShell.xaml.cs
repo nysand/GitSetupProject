038573a833bfa557561769b5c39ad548b5c1bfc8
nysand <46599021+nysand@users.noreply.github.com>
2022-11-09 14:25:50 +0000

    Add files via upload

---
+++ GitSetupProject/GitSetupProject/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using GitSetupProject.ViewModels;
+using GitSetupProject.Views;
+using Xamarin.Forms;
+
+namespace GitSetupProject
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
+
---
+++ GitSetupProject/GitSetupProject/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using GitSetupProject.ViewModels;
+
+namespace GitSetupProject.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
